a721d6352062f8fd37a5bca6d2e1d2f388060ad9
zumaichi <dzumaquero@gmail.com>
2026-01-25 10:32:27 +0100

    0.4 pagina de detalles implementadas

--- src/app/houses/[houseId]/page.tsx
+++ src/app/houses/[houseId]/page.tsx
@@ -1,25 +1,39 @@
 import React from 'react';
 import { Metadata } from 'next';
+import { api, House, mapHouseListFromApiToVm } from '#pods/house-list';
+
+export async function generateStaticParams() {
+  return [{ houseId: '1' }, { houseId: '2' }, { houseId: '3' }];
+}
 
 interface Props {
   params: { houseId: string };
 }
 
 export const generateMetadata = async (props: Props): Promise<Metadata> => {
-  const { params } = props;
+  const params = await props.params;
   return {
-    title: `Rent a car - Car ${params.houseId} details`,
+    title: `Casa ${params.houseId} detalles`,
   };
 };
-
-const CarPage = (props: Props) => {
-  const { params } = props;
+const HousePage = async (props: Props) => {
+  const params = await props.params;
+  const houses = await api.getHouseList();
+  const house = houses.find((h) => h.id === params.houseId);
   return (
-    <>
-      <h2>House detail page</h2>
-      <p>{params.houseId}</p>
-    </>
+    <div>
+      <h1>{house?.name}</h1>
+
+      <p>{house?.description}</p>
+      <p>City: {house?.city}</p>
+      <p>Price: {house?.price}€ / noche</p>
+      <img
+        src={`http://localhost:3001${house?.image}`}
+        alt={house?.name}
+        style={{ maxWidth: '400px' }}
+      />
+    </div>
   );
 };
 
-export default CarPage;
+export default HousePage;
